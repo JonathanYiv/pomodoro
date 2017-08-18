a4fc22a719569166cacbd3cb2433d5d9c3f642c2
Jonathan Yiv <JonathanYiv@gmail.com>
2017-08-18 15:51:25 -0400

    Added notes on future fixes needed

--- js/script.js
+++ js/script.js
@@ -66,3 +66,24 @@ $(document).ready(function() {
 
 
 })
+
+/*
+Bang Ben Feedback: To Fix Later
+1) Why not
+display = $('.time')
+and
+display.text(minutes+seconds)
+These are both jQuery instead of Javascript, and both are being mixed in.
+I tried it and it doesn't work. How do I fix it?
+
+2) setInterval() queries the DOM over and over and will have a lot of callback functions.
+I need to query once at the top of the file and reuse references.
+Should use something like:
+var pauseButton = $('.fa-pause');
+pauseButton.onClick = function() {};
+
+3) To keep separation of functionality, style, and accessibility:
+<a href="" class="js-play" title="Start Timer"><i class="fa fa-play fa-5x"></i></a>
+
+4) The Timer isn't actually One Second
+*/
